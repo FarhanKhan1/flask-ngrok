f845157004b8f6933fc2c6a6326506e8ca9fa221
Farhankhan1 <enggfarhan36@gmail.com>
2022-12-02 10:23:09 +0500

    This is second commit

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-# from flask import Flask
-# from flask_restful import Resource, Api
-# from flask_ngrok import run_with_ngrok
-# app = Flask(__name__)
-# api = Api(app)
-# run_with_ngrok(app)
-
-# class HelloWorld(Resource):
-#     def get(self):
-#         return {'hello': 'world'}
-
-# api.add_resource(HelloWorld, '/')
-
-# if __name__ == '__main__':
-#     app.run()
-
-
 from flask import Flask
 from flask_ngrok import run_with_ngrok
 import webbrowser
